058f295bd3b0e8580b90e93e10a7dc11e4204be9
Stevo.Men <stevemenard3@gmail.com>
2024-05-08 22:17:55 -0400

    Basic setup of home page

---
+++ javascript/product-grabber.js
@@ -0,0 +1,15 @@
+async function fetchProducts() {
+    try {
+      const response = await fetch('https://dummyjson.com/products');
+      const json = await response.json();
+      const products = json.products;
+      console.log(products);
+      return products;
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      return null;
+    }
+  }
+
+
+  fetchProducts();
