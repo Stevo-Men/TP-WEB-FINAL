468fbc70f5ebdbef7af07b89801f836931f5524e
Stevo.Men <stevemenard3@gmail.com>
2024-05-26 00:25:18 -0400

    Fully completed account page

--- javascript/account.js
+++ javascript/account.js
@@ -1,18 +1,14 @@
-
 function getUserFromLocalStorage() {
     const user = JSON.parse(localStorage.getItem('user'));
     return user;
 }
 
-
 function updateAccountLink() {
     const user = getUserFromLocalStorage();
-    const accountLink = document.querySelector('.account-link');
-    const accountFirstName = document.getElementById('firstName');
+    const accountImg = document.getElementById('user-img');
 
-    if (user && accountLink) {
-        accountLink.innerHTML = `<h2 class="user-name">${user.firstName}  ${user.lastName}</h2>`;
-  
+    if (user && user.image) {
+        accountImg.src = user.image;
     }
 }
 
@@ -23,16 +19,61 @@ function updatePlaceholder() {
     const usernameInput = document.getElementById('userName');
     const emailInput = document.getElementById('email');
 
-    if (user && user.firstName && firstNameInput) {
-        firstNameInput.placeholder = `${user.firstName}`;
-        lastNameInput.placeholder =  `${user.lastName}`;
-        usernameInput.placeholder =  `${user.username}`;
-        emailInput.placeholder =  `${user.email}`;
-        
+    if (user) {
+        if (user.firstName && firstNameInput) {
+            firstNameInput.value = user.firstName;
+            firstNameInput.placeholder = `${user.firstName}`;
+        }
+
+        if (user.lastName && lastNameInput) {
+            lastNameInput.value = user.lastName;
+            lastNameInput.placeholder = `${user.lastName}`;
+        }
+
+        if (user.username && usernameInput) {
+            usernameInput.value = user.username;
+            usernameInput.placeholder = `${user.username}`;
+        }
+
+        if (user.email && emailInput) {
+            emailInput.value = user.email;
+            emailInput.placeholder = `${user.email}`;
+        }
     }
+}
+
+function redirectToIndex() {
+    window.location.href = 'index.html';
+  }
+
+function updateUserInLocalStorage() {
+    const firstNameInput = document.getElementById('firstName');
+    const lastNameInput = document.getElementById('lastName');
+    const usernameInput = document.getElementById('userName');
+    const emailInput = document.getElementById('email');
+
+    const updatedUser = {
+        firstName: firstNameInput.value,
+        lastName: lastNameInput.value,
+        username: usernameInput.value,
+        email: emailInput.value,
+        image: getUserFromLocalStorage().image 
+    };
+
+    localStorage.setItem('user', JSON.stringify(updatedUser));
+}
+
+function showSuccessBox() {
+
+    document.getElementById('success-box').style.display = 'block';
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     updateAccountLink();
     updatePlaceholder();
+
+    const updateButton = document.getElementById('updateButton');
+    if (updateButton) {
+        updateButton.addEventListener('click', updateUserInLocalStorage);
+    }
 });
